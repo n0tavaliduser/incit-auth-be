b50c708b170c39b010e94491eebf2dd20883864f
n0tavaliduser <stevanoputra38@gmail.com>
2024-10-31 08:58:42 +0700

    refactor: knexfile proper db config

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,11 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT || 3306,
-      charset: 'utf8mb4'
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'incit_test_db',
+      port: process.env.DB_PORT || 3306
     },
     migrations: {
       directory: './database/migrations',
